d686ffc9bced17bd6b5b14c8d017647c6ac546fe
paulkreft <paulykreft@gmail.com>
2024-01-15 16:07:31 +0100

    code regular tasks

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
-const numbers: number[] = [15, 42, 8, 91, 37, 24, 63, 50, 11, 77];
-const words: string[] = ["apple", "banana", "orange", "grape", "kiwi", "melon"];
+const numbers: number[] = [15, 42, 8, 91, 37, 24, 63, 50, 11, 77]
+const words: string[] = ["apple", "banana", "orange", "grape", "kiwi", "melon"]
 
-console.log(numbers.map((num: number) => num * 2));
-console.log(words.filter((word: string) => word.length > 5));
-console.log(numbers.reduce((prev: number, curr: number) => (prev + curr), 0))
+console.log("%cFirst Part:", "font-weight: bold")
+console.log(numbers.map((num: number) => num * 2))
+console.log(words.filter((word: string) => word.length > 5))
+console.log(numbers.reduce((acc: number, curr: number) => (acc + curr), 0))
 console.log(numbers.some((num: number) => num > 10))
